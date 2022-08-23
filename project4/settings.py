dbdac01c7e1aa8854e4dc58f5eb6376c33e74327
Dini <ioluwande@gmail.com>
2022-08-23 22:58:42 +0100

    teting

--- project4/settings.py
+++ project4/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'network/static'),)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'network/static'),)
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_on_heroku.settings(locals(), staticfiles = False)
+# django_on_heroku.settings(locals(), staticfiles = False)
